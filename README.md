13de50b5f3ca92b8e5945ebe8a6dae60d785127f
williardt <609353@bk.ru>
2026-03-23 17:15:25 +0300

    DecodeAndValidateRequest edits

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ first start:
 
     make env-up
 
+    make migrate-up
+
+    make env-port-forward
+
+    make app-run
+
+ ---   
+
 все команды в project/Makefile
--- internal/core/transport/http/request/decode.go
+++ internal/core/transport/http/request/decode.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	core_errors "github.com/WilliardT/go-mvp/internal/core/errors"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -15,13 +16,20 @@ func DecodeAndValidateRequest(
 	dest any,
 ) error {
 	if err := json.NewDecoder(r.Body).Decode(dest); err != nil {
-		return fmt.Errorf("decode json: %w", err)
+		return fmt.Errorf(
+			"decode json: %v: %w",
+			err,
+			core_errors.ErrInvalidArgument,
+		)
 	}
 
 	if err := requestValidator.Struct(dest); err != nil {
-		return fmt.Errorf("request validation: %w", err)
+		return fmt.Errorf(
+			"request validation: %v: %w", 
+			err,
+			core_errors.ErrInvalidArgument,
+		)
 	}
 
 	return nil
 }
-
