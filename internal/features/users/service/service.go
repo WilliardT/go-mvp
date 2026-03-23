bbe441abd39a2ad60cec93812345887d47436895
williardt <609353@bk.ru>
2026-03-23 18:32:52 +0300

    GetUsers service

--- internal/features/users/service/service.go
+++ internal/features/users/service/service.go
@@ -16,6 +16,12 @@ type UsersRepository interface {
 		ctx context.Context,
 		user domain.User,
 	) (domain.User, error)
+
+	GetUsers(
+		ctx context.Context,
+		limit *int,
+		offset *int,
+	) ([]domain.User, error)
 }
 
 func NewUsersService(
