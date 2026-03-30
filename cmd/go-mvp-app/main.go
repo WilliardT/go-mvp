f2d65db65eb61491b753c0808eb785d5e1f64f7d
williardt <609353@bk.ru>
2026-03-30 07:27:35 +0300

    CORS  Middleware

--- cmd/go-mvp-app/main.go
+++ cmd/go-mvp-app/main.go
@@ -91,6 +91,7 @@ func main() {
 	httpServer := core_http_server.NewHTTPServer(
 		core_http_server.NewConfigMust(),
 		logger,
+		core_http_middleware.CORS(),
 		core_http_middleware.ReqestID(),
 		core_http_middleware.Logger(logger),
 		core_http_middleware.Trace(),
